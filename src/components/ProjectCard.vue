b928e8f56680133c3dacdc27e442e0d67c727755
poisnada <horberlan@gmail.com>
2024-10-21 18:24:54 -0300

    chore: add loading animation

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -7,7 +7,7 @@
     <Suspense>
       <SvgIcon class="img" :name="props.bg" margin="0" />
       <template #fallback>
-        <span class="loading skeleton">Loading...</span>
+        <span class="loading">Loading</span>
       </template>
     </Suspense>
     <div class="card-content">
@@ -85,21 +85,6 @@ function truncateText(text: string, maxLength: number) {
     z-index: 2;
   }
 
-  .skeleton {
-    width: 100%;
-    border-radius: 15px;
-
-    background-repeat: repeat-y;
-    background-size: 33.75rem 43.75rem;
-    animation: shine 1.6s infinite;
-  }
-
-  @keyframes shine {
-    to {
-      background-position: 100% 0;
-    }
-  }
-
   .img {
     width: 100%;
     height: 8rem;
@@ -139,5 +124,28 @@ function truncateText(text: string, maxLength: number) {
   justify-content: center;
   height: 10rem;
   color: white;
+  font-size: 1.5rem;
+  letter-spacing: 2px;
+  position: relative;
+}
+
+@keyframes dots {
+  0% {
+    content: "";
+  }
+  33% {
+    content: ".";
+  }
+  66% {
+    content: "..";
+  }
+  100% {
+    content: "...";
+  }
+}
+
+.loading::after {
+  content: "";
+  animation: dots 500ms steps(1, end) infinite;
 }
 </style>
