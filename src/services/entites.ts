d738db3743a9ab6249b0114fbdced2db1e15b295
poisnada <horberlan@gmail.com>
2024-10-23 02:21:24 -0300

    feat(rest): add update-snippet-star router patch

--- src/services/entites.ts
+++ src/services/entites.ts
@@ -2,40 +2,40 @@ import { PROJECT_TYPE } from "@/utils/enums/project";
 import axios from "axios";
 
 export interface Project {
-    _id: string
-    name: string
-    project_description: string
-    background: string
-    href: string
-    type: PROJECT_TYPE
+  _id: string;
+  name: string;
+  project_description: string;
+  background: string;
+  href: string;
+  type: PROJECT_TYPE;
 }
 export interface Snippet {
-  _id: string
-  name: string
-  updated_date: string
-  snippet: string
-  details: string
-  stars: number
-  avatar_url: string
-};
+  _id: string;
+  name: string;
+  updated_date: string;
+  snippet: string;
+  details: string;
+  stars: number;
+  avatar_url: string;
+}
 
 export interface FormMessage {
-  _id: string
-  name: string
-  email: string
-  message: string
+  _id: string;
+  name: string;
+  email: string;
+  message: string;
 }
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
 
-export const getProjects = async (
-  param: Array<String>
-): Promise<Project[]> => {
+export const getProjects = async (param: Array<String>): Promise<Project[]> => {
   try {
     const { data } = await api.get(
-      `/projects?type=${param.length ? Object.values(param).join(";") : PROJECT_TYPE.ALL}`
+      `/projects?type=${
+        param.length ? Object.values(param).join(";") : PROJECT_TYPE.ALL
+      }`
     );
     return data;
   } catch (error) {
@@ -53,9 +53,13 @@ export const getSnippet = async () => {
   }
 };
 
-export const updateSnippet = async (snippet: Snippet): Promise<Snippet | undefined> => {
+export const updateSnippetStars = async (
+  snippet: Snippet
+): Promise<Snippet | undefined> => {
   try {
-    const { data } = await api.put(`/snippets/${snippet._id}`, snippet);
+    const { data } = await api.patch(`/snippets/${snippet._id}/stars`, {
+      stars: snippet.stars,
+    });
     return data;
   } catch (error) {
     console.log(error);
