248f0960ecfb74fecb91db9380da95617ff56355
poisnada <horberlan@gmail.com>
2024-10-23 02:22:00 -0300

    chore: edit snippet code highlight package

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
-import "./assets/main.css";
+import "./assets/main.scss";
 
 import App from "./App.vue";
 import { Quasar } from "quasar";
-import VueCodeHighlight from "vue-code-highlight";
+// import VueCodeHighlight from "vue-code-highlight";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import router from "./router";
 import { VueQueryPlugin } from "@tanstack/vue-query";
-
+import "highlight.js/styles/stackoverflow-dark.css";
+import "highlight.js/lib/common";
+import hljsVuePlugin from "@highlightjs/vue-plugin";
 const app = createApp(App);
 
 app.use(createPinia());
-app.use(VueCodeHighlight);
+app.use(hljsVuePlugin);
 app.use(Quasar, {
   plugins: {},
 });
