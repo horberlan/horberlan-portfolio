710e1daff03208379a6bb4d515e612ca9d90be71
poisnada <horberlan@gmail.com>
2024-10-30 17:22:49 -0300

    fix: router component typo

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import AboutViewVue from "@/views/about/AboutView.vue";
+import AboutView from "@/views/about/AboutView.vue";
 import ContactView from "@/views/contact/ContactView.vue";
-import IndexHomeVue from "@/views/home/IndexHome.vue";
+import HomeView from "@/views/home/IndexHome.vue";
 import ProjectView from "@/views/projects/ProjectView.vue";
+
 const router = createRouter({
   history: createWebHistory(), // import.meta.env.BASE_URL
   routes: [
     {
       path: "/",
       name: "",
-      component: IndexHomeVue,
+      component: HomeView,
     },
     {
       path: "/",
       name: "home",
-      component: IndexHomeVue,
+      component: HomeView,
     },
     {
       path: "/about",
       name: "about",
-      component: AboutViewVue,
+      component: AboutView,
     },
     {
       path: "/projects",
